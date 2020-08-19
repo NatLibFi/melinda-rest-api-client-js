c27af94f867db3c9d551b468f8a2213399de00b9
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-08-19 10:47:49 +0300

    transform-runtime and debug update

--- src/api-client.js
+++ src/api-client.js
@@ -2,14 +2,14 @@ import fetch from 'node-fetch';
 import httpStatus from 'http-status';
 import {URL} from 'url';
 import {Error as ApiError, generateAuthorizationHeader} from '@natlibfi/melinda-commons';
-import {createLogger} from '@natlibfi/melinda-backend-commons';
+import createDebugLogger from 'debug';
 import {MarcRecord} from '@natlibfi/marc-record';
 
 // Change to true when working
 MarcRecord.setValidationOptions({subfieldValues: false});
 
 export function createApiClient({restApiUrl, restApiUsername, restApiPassword, cataloger = false, userAgent = 'Melinda commons API client / Javascript'}) {
-  const logger = createLogger();
+  const debug = createDebugLogger('@natlibfi/melinda-rest-api-client:api-client');
   const Authorization = generateAuthorizationHeader(restApiUsername, restApiPassword);
 
   const defaultParamsBulk = cataloger ? {pCatalogerIn: cataloger} : {};
@@ -20,37 +20,37 @@ export function createApiClient({restApiUrl, restApiUsername, restApiPassword, c
   };
 
   function read(recordId) {
-    logger.log('silly', 'Reading record');
+    debug('Reading record');
     return doRequest({method: 'get', path: recordId});
   }
 
   function create(record, params = {noop: 0, unique: 0}) {
-    logger.log('silly', 'Posting create');
+    debug('Posting create');
     return doRequest({method: 'post', path: '', params: {...defaultParamsPrio, ...params}, body: JSON.stringify(record, undefined, '')});
   }
 
   function update(record, id, params = {noop: 0, unique: 0}) {
-    logger.log('silly', `Posting update ${id}`);
+    debug(`Posting update ${id}`);
     return doRequest({method: 'post', path: id, params: {...defaultParamsPrio, ...params}, body: JSON.stringify(record, undefined, '')});
   }
 
   function createBulk(stream, streamContentType, params) {
-    logger.log('silly', 'Posting bulk');
+    debug('Posting bulk');
     return doRequest({method: 'post', path: 'bulk/', params: {...defaultParamsBulk, ...params}, contentType: streamContentType, body: stream});
   }
 
   function readBulk(params) {
-    logger.log('silly', 'Reading bulk metadata');
+    debug('Reading bulk metadata');
     return doRequest({method: 'get', path: 'bulk/', params});
   }
 
   async function doRequest({method, path, contentType = 'application/json', params = false, body = null}) {
-    logger.log('silly', 'Doing request');
+    debug('Executing request');
     try {
       const query = params ? new URLSearchParams(params) : '';
       const url = new URL(`${restApiUrl}${path}${query === '' ? '' : '?'}${query}`);
 
-      logger.log('debug', `connection URL ${url.toString()}`);
+      debug(`connection URL ${url.toString()}`);
 
       const response = await fetch(url, {
         method,
@@ -63,18 +63,18 @@ export function createApiClient({restApiUrl, restApiUsername, restApiPassword, c
         body
       });
 
-      logger.log('http', `${path === 'bulk/' ? 'Bulk' : 'Prio'} ${method} status: ${response.status}`);
+      debug(`${path === 'bulk/' ? 'Bulk' : 'Prio'} ${method} status: ${response.status}`);
 
       if (response.status === httpStatus.OK || response.status === httpStatus.CREATED) {
         if (path === '') {
           // Create new record
           const recordId = response.headers.get('Record-ID') || undefined;
-          logger.log('silly', `Response data: ${JSON.stringify(recordId)}`);
+          debug(`Response data: ${JSON.stringify(recordId)}`);
           return {recordId};
         }
 
         const data = await response.json();
-        logger.log('silly', `Response data: ${JSON.stringify(data)}`);
+        debug(`Response data: ${JSON.stringify(data)}`);
 
         if (path === 'bulk/') {
           if (method === 'post') {
@@ -96,15 +96,16 @@ export function createApiClient({restApiUrl, restApiUsername, restApiPassword, c
         return data;
       }
 
-      logger.log('error', response);
+      debug('Invalid response');
+      debug(JSON.stringify(response));
       throw new ApiError(response.status);
     } catch (error) {
-      logger.log('debug', 'Api-client Error');
+      debug('Api-client Error');
       if (error instanceof ApiError) { // eslint-disable-line functional/no-conditional-statement
         throw error;
       }
 
-      logger.log('error', error);
+      debug(JSON.stringify(error));
       throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, 'Unexpected internal error');
     }
   }
